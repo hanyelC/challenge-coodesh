4b11303b0527e56334e52bdd6350d9c3a8555f8c
hanyelC <hanyel.chamon@gmail.com>
2022-08-02 09:02:26 -0300

    feat(routes): create the api router

---
+++ src/api/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require('express')
+const { productsRoutes } = require('./products.routes')
+
+const routes = Router()
+
+routes.get('/', (req, res) => {
+  res.status(200).json({ message: 'Fullstack Challenge 20201026' })
+})
+
+routes.use('/products', productsRoutes)
+
+module.exports = { routes }
