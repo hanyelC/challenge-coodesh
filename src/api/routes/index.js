d31680ff0d0cdf65ba48066e58b4984cc8bf5a47
hanyelC <hanyel.chamon@gmail.com>
2022-08-03 08:51:17 -0300

    chore: configurating to run on production

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,6 +4,7 @@ const { productsRoutes } = require('./products.routes')
 const routes = Router()
 
 routes.get('/', (req, res) => {
+  console.log('here')
   res.status(200).json({ message: 'Fullstack Challenge 20201026' })
 })
 
